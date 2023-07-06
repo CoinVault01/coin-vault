f4c36998b2b93e420d67a416fcc96b711cf5d980
TheRealAdegoke <adewaleadegoke0210@gmail.com>
2023-07-06 13:01:55 +0100

    Registration In Progress

--- src/Pages/SignUp/SignUP-Components/LegalName.jsx
+++ src/Pages/SignUp/SignUP-Components/LegalName.jsx
@@ -1,114 +1,112 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import EmailPassword from './EmailPassword';
 
-const LegalName = ({username}) => {
-    const [firstName, setFirstName] = useState("");
-    const [lastName, setLastName] = useState("");
-    const [showForm, setShowForm] = useState(false);
 
-    const handleInputChange1 = (event) => {
-      setFirstName(event.target.value);
-      setShowForm(false)
-    };
+const LegalName = ({onNext, onPrevious, userName}) => {
+   const [firstName, setFirstName] = useState("");
+   const [lastName, setLastName] = useState("");
 
-    const handleInputChange2 = (event) => {
-      setLastName(event.target.value);
-      setShowForm(false);
-    };
+   const handleInputChange1 = (event) => {
+     setFirstName(event.target.value);
+   };
 
-    const isContinueDisabled = firstName === "" || lastName === "";
+   const handleInputChange2 = (event) => {
+     setLastName(event.target.value);
+   };
 
-    const handleContinueClick = () => {
-       if (!isContinueDisabled) {
-         setShowForm(true);
-       }
-    };
+   const isContinueDisabled = firstName === "" || lastName === "";
 
+   const handleContinueClick = () => {
+     if (!isContinueDisabled) {
+       onNext(firstName);
+     }
+   };
 
   return (
-    <section>
-      {showForm ? (
-        <EmailPassword firstName={firstName} />
-      ) : (
+    <section className="">
+      <div>
+        <div className="mb-[30px]">
+          <h1 className="text-[25px] font-[600] font-[poppins] capitalize">
+            Hey {userName}👋
+          </h1>
+          <p className="pb-[5px] text-[25px] font-[600] font-[poppins] capitalize smallerDevice:text-[25px]">
+            Please give us your legal name
+          </p>
+        </div>
+
         <div>
-          <div className="mb-[30px]">
-            <h1 className="text-[25px] font-[600] font-[poppins]">
-              Hey {username}👋
-            </h1>
-            <p className="pb-[5px] text-[25px] font-[600] font-[poppins] capitalize smallerDevice:text-[25px]">
-              Please give us your legal name
-            </p>
-          </div>
-
-          <div>
-            <div className="mb-[20px]">
-              <label
-                htmlFor="lastName"
-                className="capitalize text-[rgb(157,166,177)] font-[600] pb-[3px] inline-block"
-              >
-                first name
-              </label>
-
-              <div className="border-[1px] border-[rgba(255,255,255,0.2)] rounded-[5px] h-[45px] bg-[rgb(32,37,43)] flex items-center max-w-[400px]">
-                <div className="mx-[20px]">
-                  <i className="fa-solid fa-user text-[rgb(157,166,177)]"></i>
-                </div>
-                <input
-                  type="text"
-                  className="user-input w-[100%] h-[100%] bg-[rgb(32,37,43)] pl-[20px] pb-[3px] pr-[20px] mr-[2px] font-[600]"
-                  placeholder="e.g John"
-                  onChange={handleInputChange1}
-                  value={firstName}
-                />
-              </div>
-            </div>
-
-            <div className="">
-              <label
-                htmlFor="lastName"
-                className="capitalize text-[rgb(157,166,177)] font-[600] pb-[3px] inline-block"
-              >
-                last name
-              </label>
-
-              <div className="border-[1px] border-[rgba(255,255,255,0.2)] rounded-[5px] h-[45px] bg-[rgb(32,37,43)] flex items-center max-w-[400px]">
-                <div className="mx-[20px]">
-                  <i className="fa-solid fa-user text-[rgb(157,166,177)]"></i>
-                </div>
-                <input
-                  type="text"
-                  className="user-input w-[100%] h-[100%] bg-[rgb(32,37,43)] pl-[20px] pb-[3px] pr-[20px] mr-[2px] font-[600]"
-                  placeholder="e.g Doe"
-                  onChange={handleInputChange2}
-                  value={lastName}
-                />
-              </div>
-            </div>
-          </div>
-
-          <div className="mt-[40px] max-w-[400px] flex justify-center">
-            <button
-              className="form-btn block w-[100%] font-[600] py-[10px] mb-[20px] rounded-[8px]"
-              onClick={handleContinueClick}
+          <div className="mb-[20px]">
+            <label
+              htmlFor="lastName"
+              className="capitalize text-[rgb(157,166,177)] font-[600] pb-[3px] inline-block"
             >
-              Continue
-            </button>
+              first name
+            </label>
+
+            <div className="border-[1px] border-[rgba(255,255,255,0.2)] rounded-[5px] h-[45px] bg-[rgb(32,37,43)] flex items-center max-w-[400px]">
+              <div className="mx-[20px]">
+                <i className="fa-solid fa-user text-[rgb(157,166,177)]"></i>
+              </div>
+              <input
+                type="text"
+                className="user-input w-[100%] h-[100%] bg-[rgb(32,37,43)] pl-[20px] pb-[3px] pr-[20px] mr-[2px] font-[600] capitalize"
+                placeholder="John"
+                onChange={handleInputChange1}
+                value={firstName}
+              />
+            </div>
           </div>
 
-          <div>
-            <p className="pb-[20px] text-[rgba(255,255,255,0.5)]">
-              Already have an account?{" "}
-              <Link to="/login">
-                <span className="text-[rgb(160,210,254)]">Sign In</span>
-              </Link>{" "}
-              <i className="fa-solid fa-reply-all text-[white]"></i>
-            </p>
+          <div className="">
+            <label
+              htmlFor="lastName"
+              className="capitalize text-[rgb(157,166,177)] font-[600] pb-[3px] inline-block"
+            >
+              last name
+            </label>
+
+            <div className="border-[1px] border-[rgba(255,255,255,0.2)] rounded-[5px] h-[45px] bg-[rgb(32,37,43)] flex items-center max-w-[400px]">
+              <div className="mx-[20px]">
+                <i className="fa-solid fa-user text-[rgb(157,166,177)]"></i>
+              </div>
+              <input
+                type="text"
+                className="user-input w-[100%] h-[100%] bg-[rgb(32,37,43)] pl-[20px] pb-[3px] pr-[20px] mr-[2px] font-[600] capitalize"
+                placeholder="Doe"
+                onChange={handleInputChange2}
+                value={lastName}
+              />
+            </div>
           </div>
         </div>
-      )}
+
+        <div className="mt-[40px] max-w-[400px] gap-[5px] flex justify-center">
+          <button
+            className="form-btn block w-[100%] font-[600] py-[10px] mb-[20px] rounded-[8px]"
+            onClick={onPrevious}
+          >
+            Previous
+          </button>
+          <button
+            className="form-btn block w-[100%] font-[600] py-[10px] mb-[20px] rounded-[8px]"
+            onClick={handleContinueClick}
+          >
+            Continue
+          </button>
+        </div>
+
+        <div>
+          <p className="pb-[20px] text-[rgba(255,255,255,0.5)]">
+            Already have an account?{" "}
+            <Link to="/login">
+              <span className="text-[rgb(160,210,254)]">Sign In</span>
+            </Link>{" "}
+            <i className="fa-solid fa-reply-all text-[white]"></i>
+          </p>
+        </div>
+      </div>
     </section>
   );
-}
+};
 
-export default LegalName
+export default LegalName;
---
+++ src/Pages/SignUp/SignUP-Components/UserName.jsx
@@ -0,0 +1,118 @@
+import React, { useState } from "react";
+import coinVault from "../SignUp-Image/coin-bg.png";
+import { Link } from "react-router-dom";
+
+const Username = ({onNext}) => {
+    const [inputText, setInputText] = useState("");
+    const [isChecked, setIsChecked] = useState(false);
+
+    const handleInputChange = (event) => {
+      setInputText(event.target.value);
+    };
+
+    const handleCheckboxChange = () => {
+      setIsChecked(!isChecked);
+    };
+
+    const handleContinueClick = () => {
+      if (inputText.trim() !== "" && isChecked) {
+        onNext(inputText)
+        
+      }
+    };
+
+    const handleFormSubmit = (event) => {
+      event.preventDefault();
+    };
+
+    const isDisabled = inputText.trim() === "" || !isChecked;
+    const buttonStyle = isDisabled
+      ? { cursor: "not-allowed", filter: "opacity(0.5)" }
+      : {};
+
+    
+
+  return (
+    <section className="ml-[20px]">
+      <div>
+        <div className="mb-[30px]">
+          <h1 className="text-[30px] font-[600] font-[poppins]">Hey 👋</h1>
+          <p className="pb-[5px] text-[28px] font-[600] font-[poppins] capitalize smallerDevice:text-[25px]">
+            welcome to CoinVault
+          </p>
+          <p className="text-[rgb(157,166,177)] font-[600]">
+            Let’s get to know you!
+          </p>
+          <p className="text-[rgb(157,166,177)] font-[600] pr-[50px] smallerDevice:pr-[0]">
+            We’ll need you to choose a really cool name that other users can
+            find you with, choose something cool like aquaman, or deadpool 😜
+          </p>
+        </div>
+      </div>
+
+      <div>
+        <div>
+          <label
+            htmlFor="username"
+            className="capitalize text-[rgb(157,166,177)] font-[600] pb-[3px] inline-block"
+          >
+            username
+          </label>
+
+          <div className="border-[1px] border-[rgba(255,255,255,0.2)] rounded-[5px] h-[45px] bg-[rgb(32,37,43)] flex items-center max-w-[400px]">
+            <div className="mx-[20px]">
+              <i className="fa-solid fa-user text-[rgb(157,166,177)]"></i>
+            </div>
+            <input
+              type="text"
+              className="user-input w-[100%] h-[100%] bg-[rgb(32,37,43)] pl-[20px] pb-[3px] pr-[20px] mr-[2px] font-[600] capitalize"
+              placeholder="Enter Username"
+              value={inputText}
+              onChange={handleInputChange}
+            />
+          </div>
+
+          <div className="flex gap-[10px] ml-[15px] mt-[15px]">
+            <input
+              type="checkbox"
+              name=""
+              id=""
+              className=""
+              checked={isChecked}
+              onChange={handleCheckboxChange}
+            />
+            <p className="font-[600]">
+              I agree to CoinVault's{" "}
+              <span className="text-[rgb(160,210,254)]">
+                Terms & conditions
+              </span>
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="mt-[40px] max-w-[400px] flex justify-center">
+        <button
+          className="form-btn block w-[100%] font-[600] py-[10px] mb-[20px] rounded-[8px]"
+          style={buttonStyle}
+          disabled={isDisabled}
+          onClick={handleContinueClick}
+        >
+          Continue
+        </button>
+      </div>
+
+      <div>
+        <p className="pb-[20px] text-[rgba(255,255,255,0.5)]">
+          Already have an account?{" "}
+          <Link to="/login">
+            <span className="text-[rgb(160,210,254)]">Sign In</span>
+          </Link>{" "}
+          <i class="fa-solid fa-reply-all text-[white]"></i>
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Username;
