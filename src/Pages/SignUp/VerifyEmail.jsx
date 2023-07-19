6bc9ff1d85522e2de8e8c107e1fbdf2c29e0352e
TheRealAdegoke <adewaleadegoke0210@gmail.com>
2023-07-19 12:12:20 +0100

    User Can successfully register and verify email

--- src/Pages/SignUp/VerifyEmail.jsx
+++ src/Pages/SignUp/VerifyEmail.jsx
@@ -177,10 +177,10 @@ const VerifyEmail = () => {
     <section className="formAnim bg-[rgb(28,33,39)] text-[white] min-h-[100vh]">
       <ToastContainer hideProgressBar autoClose={3000} />
       <div className=" w-[90%] max-w-[500px] mx-auto pt-[100px] smallerDevice:pt-[30px]">
-        <h1 className="font-[600] text-[32px] smallerDevice:text-[25px] smallDevice:text-[30px] mb-[10px]">
+        <h1 className="font-[600] text-[32px] smallerDevice:text-[25px] smallDevice:text-[30px] ml-[20px] mb-[10px]">
           Verify your email address
         </h1>
-        <p className="text-[rgb(157,166,177)] font-[600]">
+        <p className="text-[rgb(157,166,177)] ml-[20px] font-[600]">
           A six digit pin has been sent to {savedEmail} enter into this field to
           complete your account creation process
         </p>
@@ -188,33 +188,37 @@ const VerifyEmail = () => {
         <div className="my-[40px]">
           <label
             htmlFor="password"
-            className="capitalize text-[rgb(157,166,177)] font-[600] pb-[10px] pl-[15px] inline-block"
+            className="capitalize text-[rgb(157,166,177)] font-[600] pb-[10px] pl-[28px] inline-block"
           >
             Enter code
           </label>
 
-          <PinInput
-            id="verificationCode"
-            length={6}
-            initialValue=""
-            secret
-            secretDelay={100}
-            inputMode="number"
-            style={{ padding: "10px" }}
-            inputStyle={{
-              borderColor: "rgba(255,255,255,0.5)",
-              borderTopRightRadius: "5px",
-              borderTopLeftRadius: "5px",
-              borderBottomLeftRadius: "5px",
-              borderBottomRightRadius: "5px",
-              background: "transparent",
-              marginRight: "10px",
-            }}
-            inputFocusStyle={{ borderColor: "rgba(255,255,255,0.5)" }}
-            autoSelect={true}
-            regexCriteria={/^[ A-Za-z0-9_@./#&+-]*$/}
-            onChange={(value) => setVerificationCode(value)}
-          />
+          <div className="ml-[20px]">
+            <PinInput
+              id="verificationCode"
+              length={6}
+              initialValue=""
+              secret
+              secretDelay={100}
+              inputMode="number"
+              style={{ padding: "5px" }}
+              inputStyle={{
+                borderColor: "rgba(255,255,255,0.5)",
+                borderTopRightRadius: "5px",
+                borderTopLeftRadius: "5px",
+                borderBottomLeftRadius: "5px",
+                borderBottomRightRadius: "5px",
+                background: "transparent",
+                marginRight: "5px",
+                width: "39px",
+                height: "40px"
+              }}
+              inputFocusStyle={{ borderColor: "rgba(255,255,255,0.5)" }}
+              autoSelect={true}
+              regexCriteria={/^[ A-Za-z0-9_@./#&+-]*$/}
+              onChange={(value) => setVerificationCode(value)}
+            />
+          </div>
         </div>
 
         <button
