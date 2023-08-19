c65313342ba62d13e5707a5fd594b2e748a8d763
Adegoke Gabriel Adewale <111113715+TheRealAdegoke@users.noreply.github.com>
2023-08-19 11:28:24 +0100

    User Profile Image added to the top header

--- src/DashBoard/Settings/ProfileSettings.jsx
+++ src/DashBoard/Settings/ProfileSettings.jsx
@@ -1,10 +1,17 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { LongCard } from "../../Skeleton/Skeleton";
+import UpdateName from "./UpdateName";
 
 const ProfileSettings = () => {
   const [userData, setUserData] = useState(null);
   const [isLoading, setIsLoading] = useState(true); // New state to track loading
+  const [updateNameModal, setUpdateNameModal] = useState(false);
+
+  const handleUpdateNameModal = () => {
+    setUpdateNameModal(true);
+  };
+
 
   useEffect(() => {
     // Fetch user data from the backend using the JWT token
@@ -46,14 +53,24 @@ const ProfileSettings = () => {
           </p>
 
           <div className="border-[1px] border-[rgb(50,56,63)] rounded-[10px] bg-[rgb(32,37,43)]">
-            <div className="border-b-[1px] border-[rgb(50,56,63)] w-[90%] mx-auto my-[20px]">
-              <h1 className="font-[poppins] font-[600] text-[rgb(165,177,189)] mb-[5px]">
-                Full Name
-              </h1>
+            <div className="border-b-[1px] border-[rgb(50,56,63)] w-[90%] mx-auto my-[20px] flex justify-between items-center">
+              <div>
+                <h1 className="font-[poppins] font-[600] text-[rgb(165,177,189)] mb-[5px]">
+                  Full Name
+                </h1>
 
-              <p className="font-[poppins] font-[500] text-[rgb(165,177,189)] mb-[5px]">
-                {userData && userData.firstName} {userData && userData.lastName}
-              </p>
+                <p className="font-[poppins] font-[500] text-[rgb(165,177,189)] mb-[5px]">
+                  {userData && userData.firstName}{" "}
+                  {userData && userData.lastName}
+                </p>
+              </div>
+
+              <div
+                className="text-[rgb(23,206,55)] cursor-pointer"
+                onClick={handleUpdateNameModal}
+              >
+                <i className="fa-solid fa-pen-to-square"></i>
+              </div>
             </div>
 
             <div className="border-b-[1px] border-[rgb(50,56,63)] w-[90%] mx-auto my-[20px]">
@@ -78,6 +95,14 @@ const ProfileSettings = () => {
           </div>
         </div>
       )}
+
+      <div>
+        <UpdateName
+          updateNameModal={updateNameModal}
+          setUpdateNameModal={setUpdateNameModal}
+          userData={userData}
+        />
+      </div>
     </section>
   );
 }
