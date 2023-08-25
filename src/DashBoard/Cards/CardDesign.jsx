e6d308b25d657215bacc73c4d16799df36f0c6a0
Adegoke Gabriel Adewale <111113715+TheRealAdegoke@users.noreply.github.com>
2023-08-25 12:00:50 +0100

    Card Design in progress

--- src/DashBoard/Cards/CardDesign.jsx
+++ src/DashBoard/Cards/CardDesign.jsx
@@ -2,25 +2,45 @@ import React, { useRef, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css/effect-cards";
 import { EffectCards } from "swiper";
-import "./Cards.css"
+import "./Cards.css";
 import firstCard from "../Cards/CardImage/CoinVault-card-1.png";
 import secondCard from "../Cards/CardImage/CoinVault-card-2.png";
 import thirdCard from "../Cards/CardImage/CoinVault-card-3.png";
 import fourthCard from "../Cards/CardImage/CoinVault-card-4.png";
 import fifthCard from "../Cards/CardImage/CoinVault-card-5.png";
+import CardModal from "./CardModal";
 
 const CardDesign = () => {
+  const [showCardDetails, setShowCardDetails] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(firstCard); 
+
+  const handleImageClick = (imageSrc) => {
+    setSelectedImage(imageSrc);
+    setShowCardDetails(false);
+  };
+
   return (
-    <section className="flex justify-center">
+    <section className="flex flex-col justify-center">
       <div
-        className={`border-[1px] bg-[rgb(32,37,43)] border-[rgb(50,56,63)] w-[90%] max-w-[1050px] mx-auto  my-[20px] rounded-[8px]`}
+        className={`${
+          showCardDetails ? "hidden" : "block"
+        } border-[1px] bg-[rgb(32,37,43)] border-[rgb(50,56,63)] w-[90%] max-w-[1050px] mx-auto  my-[20px] rounded-[8px]`}
       >
-        <div className="my-[20px]">
-          <img
-            src={firstCard}
-            alt=""
-            className="w-[80%] max-w-[350px] mx-auto rounded-[8px]"
-          />
+        <div className="mt-[20px] mb-[40px] w-[80%] max-w-[350px] mx-auto cursor-pointer">
+          {selectedImage && (
+            <img src={selectedImage} alt="" className="w-full rounded-[8px]" />
+          )}
+        </div>
+
+        <div className="mb-[20px] text-center">
+          <p
+            className="bg-[rgb(182,220,235)] rounded-full mx-auto w-[120px] py-[5px] text-[black] cursor-pointer"
+            onClick={() => {
+              setShowCardDetails(true);
+            }}
+          >
+            Show Details
+          </p>
         </div>
 
         <div className="smallDevice:w-[65%] smallerDevice:w-[63%] mx-auto max-w-[350px]  mb-[40px]">
@@ -31,7 +51,7 @@ const CardDesign = () => {
             className="mySwiper"
           >
             <SwiperSlide>
-              <div className="">
+              <div className="" onClick={() => handleImageClick(firstCard)}>
                 <img
                   src={firstCard}
                   alt=""
@@ -41,7 +61,7 @@ const CardDesign = () => {
             </SwiperSlide>
 
             <SwiperSlide>
-              <div className="">
+              <div className="" onClick={() => handleImageClick(secondCard)}>
                 <img
                   src={secondCard}
                   alt=""
@@ -51,7 +71,7 @@ const CardDesign = () => {
             </SwiperSlide>
 
             <SwiperSlide>
-              <div className="">
+              <div className="" onClick={() => handleImageClick(thirdCard)}>
                 <img
                   src={thirdCard}
                   alt=""
@@ -61,7 +81,7 @@ const CardDesign = () => {
             </SwiperSlide>
 
             <SwiperSlide>
-              <div className="">
+              <div className="" onClick={() => handleImageClick(fourthCard)}>
                 <img
                   src={fourthCard}
                   alt=""
@@ -71,7 +91,7 @@ const CardDesign = () => {
             </SwiperSlide>
 
             <SwiperSlide>
-              <div className="">
+              <div className="" onClick={() => handleImageClick(fifthCard)}>
                 <img
                   src={fifthCard}
                   alt=""
@@ -82,6 +102,12 @@ const CardDesign = () => {
           </Swiper>
         </div>
       </div>
+
+      <CardModal
+        showCardDetails={showCardDetails}
+        setShowCardDetails={setShowCardDetails}
+        selectedImage={selectedImage}
+      />
     </section>
   );
 };
---
+++ src/DashBoard/Cards/CardModal.jsx
@@ -0,0 +1,76 @@
+import React, { useRef, useState } from "react";
+import "./Cards.css";
+import firstCard from "../Cards/CardImage/CoinVault-card-1.png";
+
+const CardModal = ({ showCardDetails, setShowCardDetails, selectedImage }) => {
+  return (
+    <section
+      className={`${
+        showCardDetails ? "block" : "hidden"
+      } flex flex-col justify-center py-[10px]`}
+    >
+      <div
+        className={`border-[1px] bg-[rgb(32,37,43)] border-[rgb(50,56,63)] w-[90%] max-w-[1050px] mx-auto h-[750px] my-[20px] rounded-[8px] largeDevice:h-[500px]`}
+      >
+        <div className="flex justify-end mb-[20px] py-[10px] pr-[20px] largeDevice:hidden text-[rgb(133,209,240)]">
+          <i
+            class="fa-solid fa-x cursor-pointer"
+            onClick={() => {
+              setShowCardDetails(false);
+            }}
+          ></i>
+        </div>
+
+        <div className="flex generalDevice:flex-col largeDevice:justify-between">
+          <div className="largeDevice:w-[40%] w-full generalDevice:flex justify-center items-center generalDevice:border-b-[1px] largeDevice:border-r-[1px] border-[rgb(50,56,63)] py-[20px] largeDevice:h-[500px]">
+            {selectedImage && (
+              <img
+                src={selectedImage}
+                alt=""
+                className="cards-transform-details w-[330px] rounded-[8px]"
+              />
+            )}
+          </div>
+
+          <div className="largeDevice:w-[60%] generalDevice:my-[20px]">
+            <div className="flex justify-end mb-[20px] py-[10px] pr-[20px] generalDevice:hidden">
+              <i
+                class="fa-solid fa-x cursor-pointer text-[rgb(133,209,240)]"
+                onClick={() => {
+                  setShowCardDetails(false);
+                }}
+              ></i>
+            </div>
+
+            <div className="flex justify-between items-center border-b-[1px] border-[rgb(50,56,63)] pb-[10px] px-[20px] mb-[20px]">
+              <h3>Card Type</h3>
+              <p>CoinVault Card</p>
+            </div>
+
+            <div className="flex justify-between items-center border-b-[1px] border-[rgb(50,56,63)] pb-[10px] px-[20px] mb-[20px]">
+              <h3>Account number</h3>
+              <p>****************</p>
+            </div>
+
+            <div className="flex justify-between items-center border-b-[1px] border-[rgb(50,56,63)] pb-[10px] px-[20px] mb-[20px]">
+              <h3>Account Name</h3>
+              <p>****************</p>
+            </div>
+
+            <div className="flex justify-between items-center border-b-[1px] border-[rgb(50,56,63)] pb-[10px] px-[20px] mb-[20px]">
+              <h3>Card Number</h3>
+              <p>****************</p>
+            </div>
+
+            <div className="flex justify-between items-center largeDevice:border-b-[1px] border-[rgb(50,56,63)] px-[20px] mb-[20px]">
+              <h3>CVV</h3>
+              <p>***</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default CardModal;
