8945e757daef5f5ee73888f80472ccf64dac60c4
Adegoke Gabriel Adewale <111113715+TheRealAdegoke@users.noreply.github.com>
2023-08-26 04:32:34 +0100

    Card Design in progress

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ export default {
     extend: {screens: {
         "smallerDevice": {"min": "300px", "max": "350px"},
         "smallDevice": {"min": "350px", "max": "1000px"},
+        "bonusDevice": {"min": "400px", "max": "499px"},
         "generalDevice": {"max": "1000px"},
         "mediumDevice": {"min": "500px", "max": "1000px"},
         "tablet": {"min": "767px", "max": "1000px"},
