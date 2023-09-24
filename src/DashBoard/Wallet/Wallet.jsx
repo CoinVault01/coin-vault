83ef3ccbea84fc47d832af4b289dc7605e6cccd3
Adegoke Gabriel Adewale <111113715+TheRealAdegoke@users.noreply.github.com>
2023-09-24 12:22:58 +0100

    Users can now transfer coins to other users

--- src/DashBoard/Wallet/Wallet.jsx
+++ src/DashBoard/Wallet/Wallet.jsx
@@ -3,6 +3,7 @@ import DashboardLayout from "../DashboardLayout/DashboardLayout";
 import WalletPortfolio from "./WalletPortfolio";
 import axios from "axios";
 import "./Wallet.css"
+import { RotatingLines } from "react-loader-spinner";
 import WalletCoinList from "./WalletCoinList";
 import TransferCoinModal from "./TransferCoinModal";
 
@@ -52,26 +53,49 @@ const Wallet = () => {
 
     fetchUserCryptoData();
   }, [userData]);
+
+  
   
   return (
     <section className="bg-[rgb(28,33,39)] text-[white] pb-[20px]">
       <div>
         <DashboardLayout />
-        <div className="pt-[100px] largeDevice:ml-[230px]">
-          <WalletPortfolio userData={userData} setUserData={setUserData} />
-          <div className="largeDevice:flex gap-[40px] largeDevice:px-[40px]">
-            <WalletCoinList
-              userCryptoData={userCryptoData}
-              setSelectedCryptoData={setSelectedCryptoData}
-              setIsModalVisible={setIsModalVisible}
-            />
-            {isModalVisible && (
-              <TransferCoinModal
-                selectedCryptoData={selectedCryptoData}
-                setIsModalVisible={setIsModalVisible}
-              />
-            )}
-          </div>
+        <div>
+          {loading ? (
+            <div className="pt-[100px] largeDevice:ml-[230px] h-[100vh] mx-auto">
+              <div className="w-[30px] mx-auto">
+                <RotatingLines
+                  strokeColor="grey"
+                  strokeWidth="5"
+                  animationDuration="0.75"
+                  width="60"
+                  visible={true}
+                />
+              </div>
+            </div>
+          ) : (
+            <div className="pt-[100px] largeDevice:ml-[230px]">
+              <div>
+                <WalletPortfolio
+                  userData={userData}
+                  setUserData={setUserData}
+                />
+                <div className="largeDevice:flex gap-[40px] largeDevice:px-[40px]">
+                  <WalletCoinList
+                    userCryptoData={userCryptoData}
+                    setSelectedCryptoData={setSelectedCryptoData}
+                    setIsModalVisible={setIsModalVisible}
+                  />
+                  {isModalVisible && (
+                    <TransferCoinModal
+                      selectedCryptoData={selectedCryptoData}
+                      setIsModalVisible={setIsModalVisible}
+                    />
+                  )}
+                </div>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </section>
